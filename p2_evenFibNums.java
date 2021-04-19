60e8c713ecc602b0ff251442115978e2a0aac488
Zamran Gill <22265437+zamgill@users.noreply.github.com>
2021-04-19 02:39:49 -0400

    rename file

--- p2_evenFibNums.java
+++ p2_evenFibNums.java
@@ -3,7 +3,7 @@
  * 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
  * By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms
  */
-public class evenFibNums {
+public class p2_evenFibNums {
     public static void main(String[] args) {
         int answer = 2;
         int prevNum = 1;
