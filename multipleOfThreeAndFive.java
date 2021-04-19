fede84c8cc1df5fe02d24823255d0ae3b7b6ddc1
Zamran Gill <22265437+zamgill@users.noreply.github.com>
2021-04-19 02:36:01 -0400

    commit 1

---
+++ multipleOfThreeAndFive.java
@@ -0,0 +1,19 @@
+/**
+ * multipleOfThreeAndFive
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class multipleOfThreeAndFive {
+
+    private static int answer = 0;
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000 ; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                answer+=i;
+            }
+        }
+
+        System.out.println("Answer = " + answer);
+    }
+}
